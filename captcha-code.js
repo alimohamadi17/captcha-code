9bd852164bbead2e646d39500d8c9babd67e5377
Ali <ali_mohamadi2017@yahoo.com>
2021-09-02 19:13:00 +0430

    added all code

---
+++ captcha-code.js
@@ -0,0 +1,33 @@
+alert(
+    'welcome'
+)
+
+
+//  step one;
+
+var randomText = "";
+
+
+var randomChars = "1234567890QWERTYUIOPASDFGHJKLZXCVBNM"
+
+//step two
+// for create 6 charset for code we need for loop
+
+
+
+function Ali() {
+
+    for (var i = 0; i < 6; i++) {
+        var randomNumber = Math.floor(Math.random() * randomChars.length)
+        randomText += randomChars[randomNumber];
+    }
+
+    document.getElementById('content').innerHTML = randomText
+    randomText = ''
+
+}
+//step three
+
+
+
+
